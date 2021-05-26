f491dd54ccca181569e0b0165ea4b9a6ab2a16a6
Aleksey <bulldog.seregin@gmail.com>
2021-05-26 23:31:33 +0300

    Netty networking (WIP)

---
+++ common/src/main/java/ru/bulldog/cloudstorage/network/BaseInboundHandler.java
@@ -0,0 +1,23 @@
+package ru.bulldog.cloudstorage.network;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class BaseInboundHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		super.channelActive(ctx);
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		super.channelInactive(ctx);
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf data = (ByteBuf) msg;
+	}
+}
